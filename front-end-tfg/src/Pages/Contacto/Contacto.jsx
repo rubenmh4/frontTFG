e9d4ed1ff192e16553c627561696f0119a55a8c5
AYTOS\ruben.moron <Ruben.MORONHINOJOSA@berger-levrault.com>
2024-05-03 12:41:02 +0200

    Page Contact done with style and functions

--- front-end-tfg/src/Pages/Contacto/Contacto.jsx
+++ front-end-tfg/src/Pages/Contacto/Contacto.jsx
@@ -1,41 +1,47 @@
-import emailjs from '@emailjs/browser';
-import { useRef } from 'react';
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
+import "./Contacto.css";
 
 export const Contacto = () => {
+  const form = useRef();
 
-const form = useRef()
-
-const handleSubmit = (e)=> {
-    e.preventDefault()
-    emailjs.sendForm('service_ttdnl84','template_8k47xhe',form.current, {
-        publicKey: 'F8-5YhO-SLbXP6iZZ'
-    })
-    .then( ()=> {
-        console.log('SUCCES!')
-    },
-    (err)=> {
-        console.log(err)
-})
-
-}
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm("service_ttdnl84", "template_8k47xhe", form.current, {
+        publicKey: "F8-5YhO-SLbXP6iZZ",
+      })
+      .then(
+        () => {
+          console.log("SUCCES!");
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  };
 
   return (
-    <div className="contianer-contacto">
-      <div className="container-contacto-card">
-        <div className="form-contacto">
-            <form ref={form} onSubmit={handleSubmit}>
-                <label htmlFor="name">Nombre</label>
-                <input type="text" id="name" name="name"/> <br />
-                <label htmlFor="">Email</label>
-                <input type="email" id="email" name="email"/><br />
-                <label htmlFor="message">Mensaje</label>
-                <textarea name="message" id="message" cols="30" rows="10"></textarea>
-                <button type='submit'>Enviar</button>
-            </form>
-        </div>
-        <div></div>
-      </div>
-    </div>
+   <>
+<div className="container-contacto-principal">
+<h3>Contacto</h3>
+
+<div className="container-contacto">
+  <form className="form-contacto" ref={form} onSubmit={handleSubmit}>
+    <label htmlFor="name">Nombre </label>
+    <input type="text" id="name" name="name" placeholder="Nombre.."/>
+
+    <label htmlFor="email">Email</label>
+    <input type="email" id="email" name="email" placeholder="tuemail@gmail.com.."/>
+
+    <label htmlFor="message">Mensaje</label>
+    <textarea id="message" name="message" placeholder="¿Qué nos quieres comunicar?" ></textarea>
+
+    <button className="submit-contacto" type="submit">Enviar </button>
+  </form>
+</div>
+</div>
+
+</>
   );
 };
